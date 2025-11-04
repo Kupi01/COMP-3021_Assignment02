98f9a6cc298099efc273bd7ecd8aa74cbc73e373
rrc_w2024_kpatel <156253431+Kupi01@users.noreply.github.com>
2025-11-04 09:45:34 -0600

    Add sample.js for Super-Linter testing

---
+++ sample.js
@@ -0,0 +1,16 @@
+// sample.js
+/**
+ * Simple JavaScript example for Super-Linter testing
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Example usage
+console.log(add(10, 5));
+greet("Krupa");
